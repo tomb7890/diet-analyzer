7ca4e678d10788374246d47d8288136085f02eee
Tom Billingsley <tom.billingsley@gmail.com>
2016-12-19 18:59:51 -0500

    Implement initial version of Food.name method

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,8 +6,15 @@ class Food < ActiveRecord::Base
     allnutrients = response['nutrients']
     energy = allnutrients.select { |n| n['name'] == 'Energy' }[0]
     allmeasures = energy['measures']
-    measure = allmeasures.select {|m| m["label"] == "cup" }[0]
-    value = measure["value"].to_f
+    m = allmeasures.select {|m| m["label"] == measure  }[0]
+    value = m["value"].to_f
+    puts value
     value
+  end
+
+  def name
+    ndbno_x = ndbno.to_s.rjust(5, '0')
+    response = Usda.find(ndbno_x)
+    name = response["name"]
   end
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class FoodTest < ActiveSupport::TestCase
+  test 'correct operation of name method ' do
+    f = foods(:soymilk)
+    assert f.name == 'SILK Plain, soymilk'
+  end
+
   test "correct computation of calories of blueberries" do
     g = foods(:blueberries)
     assert g.calories.round(0) == 84.36.round(0)
   end
+
+  test "correct computation of calories of given food sample" do
+    g = foods(:raisinbran)
+    assert g.calories == 191
+  end
+
 end
