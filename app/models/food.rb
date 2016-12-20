8ea0ea7212f0f3d7e9e7cff0e7c5d1d7e23b56f3
Tom Billingsley <tom.billingsley@gmail.com>
2016-12-20 16:01:21 -0500

    Generalize calories method into a nutrients method

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,18 +1,20 @@
 class Food < ActiveRecord::Base
 
   def calories
+    nutrient('Energy')
+  end
+
+  def nutrient(arg)
     response = Usda.find(ndbno)
     allnutrients = response['nutrients']
-    energy = allnutrients.select { |n| n['name'] == 'Energy' }[0]
+    energy = allnutrients.select { |n| n['name'] == arg }[0]
     allmeasures = energy['measures']
-    m = allmeasures.select { |m| m["label"] == measure }[0]
-    value = m["value"].to_f
-    puts value
-    value
+    hash = allmeasures.select { |m| m['label'] == measure }[0]
+    value = hash['value'].to_f
   end
 
   def name
     response = Usda.find(ndbno)
-    name = response["name"]
+    name = response['name']
   end
 end
