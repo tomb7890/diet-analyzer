6c768ba99d59b0b4e442155c6ace7d6ec3a4f9dd
Tom Billingsley <tom.billingsley@gmail.com>
2016-12-19 20:51:16 -0500

    Dry up code by consolidating into Usda.find

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,20 +1,18 @@
 class Food < ActiveRecord::Base
 
   def calories
-    ndbno_x = ndbno.to_s.rjust(5, '0')
-    response = Usda.find(ndbno_x)
+    response = Usda.find(ndbno)
     allnutrients = response['nutrients']
     energy = allnutrients.select { |n| n['name'] == 'Energy' }[0]
     allmeasures = energy['measures']
-    m = allmeasures.select {|m| m["label"] == measure  }[0]
+    m = allmeasures.select { |m| m["label"] == measure }[0]
     value = m["value"].to_f
     puts value
     value
   end
 
   def name
-    ndbno_x = ndbno.to_s.rjust(5, '0')
-    response = Usda.find(ndbno_x)
+    response = Usda.find(ndbno)
     name = response["name"]
   end
 end
