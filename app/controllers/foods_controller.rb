09329aec9f056a84cf4b603a030f59264b7bd25f
Tom Billingsley <tom.billingsley@gmail.com>
2017-01-04 16:13:46 -0500

    Prepare search results for options_for_select

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,16 +1,26 @@
 class FoodsController < ApplicationController
 
+  include ApplicationHelper
+
   def index
     @foods = Food.all
   end
 
   def new
     if params[:search]
-      @items = Usda.search(params[:search])
+      @items = caching_search(params[:search])
+      @items = list_for_select_options (@items)
     end
   end
 
   def show
     @food = Food.find(params[:id])
   end
+
+  def caching_search(term)
+    Rails.cache.fetch(term, expires_in: 28.days) do
+      response = Usda.search(term)
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def list_for_select_options(hashes)
+    array = []
+    if hashes
+      hashes.each do |h|
+        array_element = []
+        array_element << h['name']
+        array_element << h['ndbno']
+        array << array_element
+      end
+    end
+    array
+  end
 end
