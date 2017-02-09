ba46081248c994500b3b5e53643381943f2545e8
Tom Billingsley <tom.billingsley@gmail.com>
2017-02-09 14:36:55 -0500

    Switch to postgresql from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161219223940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.integer  "ndbno"
     t.string   "measure"
