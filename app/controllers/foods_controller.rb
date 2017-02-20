6ccabfed88f6b220e5db970f145cd7be0d773019
Tom Billingsley <tom.billingsley@gmail.com>
2017-02-20 10:44:32 -0500

    Implement a delete food function

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -66,6 +66,12 @@ class FoodsController < ApplicationController
     end
   end
 
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_url
+  end
+
   def update_nutrients
     ndbno = params[:ndbno]
     measure = params[:measure]
