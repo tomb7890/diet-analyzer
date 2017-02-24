368936a5b66cb683f3e3990c4bf1e9c90b5243be
Tom Billingsley <tom.billingsley@gmail.com>
2017-02-24 06:17:45 -0500

    Configure logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Set log level
+  config.log_level = :fatal
 end
