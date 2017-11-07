5280305081c984702cf137154ceac2f4fe624369
Tom Billingsley <tom.billingsley@gmail.com>
2017-11-07 10:22:02 -0500

    Remove Help test until implementation

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get help" do
-    get :help
-    assert_response :success
-  end
 
   test "should get about" do
     get :about
