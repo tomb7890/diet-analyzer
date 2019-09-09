1ad966a2d44fc5c388b86fe6bdbad403389fe9c9
Tom Billingsley <tom.billingsley@gmail.com>
2019-09-09 08:27:27 -0400

    Tell TravisCI to use postgresql

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module DietAnalyzer
   class Application < Rails::Application
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib) 
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
